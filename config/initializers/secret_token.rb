8ed61e99194cf3237a096b8fb6d1851d95a39527
Aurelio Agundez <aagundez@businessaligned.com>
2011-02-26 14:09:59 -0800

    Added Rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goalwiki::Application.config.secret_token = '5c0ad35e3aff5a499887730d91474a2bcdd2eab31036673197089074876303673e384764a350b407d3820b75cae4702655716af593e75442f48fdfb666bca2ff'
